3a7de9b983538e7da3914c6f6277629d52360fb1
geachgithub <getachewamogne4@gmail.com>
2023-07-25 18:05:50 -0300

    the last digit task

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,5 +1,8 @@
+
 import random
+
 number=random.randint(-10000, 10000)
+
 l_digit=number%10
 if l_digit>5:
     text='and is greater than 5'
@@ -7,4 +10,4 @@ elif l_digit==0:
     text='and is zero'
 elif l_digit<6 and l_digit!=0:
     text='and is less than 6 and not 0'
-print("Last digit of {} is {} {} ".format(number,l_digit,text))
+print('Last digit of {} is {} {} '.format(number,l_digit,text))
