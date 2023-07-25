2e603c954c3c8f9d7db0270b2ae82d25b3682453
geachgithub <getachewamogne4@gmail.com>
2023-07-25 16:19:26 -0300

    1. Copy-Cut-Paste updated variable

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -5,3 +5,12 @@ middle_word=word[1:8]
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
+
+#Updated variable
+word="School"
+word_first_3=word[:3]
+word_last_2=word[4:]
+middle_word=word[1:5]
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
