e1676995ed9688623729f04d8d6319b08cffba89
geachgithub <getachewamogne4@gmail.com>
2023-07-26 04:41:09 -0300

    0-sum and README.md files added

---
+++ python-functions/0-sum.py
@@ -0,0 +1,7 @@
+# a function that adds two integers and returns the result
+
+def add(a,b):
+    return a+b
+print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
---
+++ python-functions/README.md
@@ -0,0 +1,1 @@
+Different functiions explained
