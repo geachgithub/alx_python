74fa57589ca4182870aa6a58e62bccc398200b5c
geachgithub <getachewamogne4@gmail.com>
2023-07-26 03:47:13 -0300

    last digit of random number

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -7,7 +7,7 @@ l_digit=number%10
 if l_digit>5:
     text='and is greater than 5'
 elif l_digit==0:
-    text='and is zero'
+    text='and is 0'
 elif l_digit<6 and l_digit!=0:
     text='and is less than 6 and not 0'
 print('Last digit of {} is {} {} '.format(number,l_digit,text))
