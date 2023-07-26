af4f308077fee14336efc791d64377024aec1acc
geachgithub <getachewamogne4@gmail.com>
2023-07-26 07:55:39 -0300

    string file added

--- python-functions/3-strings.py
+++ python-functions/3-strings.py
@@ -2,7 +2,7 @@
 
 def reverse_string(string):
     return string [::-1]
-print(reverse_string("Hello"))
+'''print(reverse_string("Hello"))
 print(reverse_string(""))
 print(reverse_string("madam"))
-print(reverse_string("Hello, world"))
+print(reverse_string("Hello, world"))'''
