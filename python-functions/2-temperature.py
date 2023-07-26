2f4191ad9ec91739e1aa1de864204e737b0cb721
geachgithub <getachewamogne4@gmail.com>
2023-07-26 05:48:34 -0300

    round function added

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,7 +1,7 @@
 #Tamprature converter function
 def convert_to_celsius(fahrenheit):
-    return round((fahrenheit-32)/1.8, ndigits=3)
-'''print(convert_to_celsius(98.6))
+    return (fahrenheit-32)/1.8
+print(convert_to_celsius(98.6))
 print(convert_to_celsius(-40))
 print(convert_to_celsius(-459.67))
-print(convert_to_celsius(32))'''
+print(convert_to_celsius(32))
