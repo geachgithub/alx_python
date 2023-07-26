78ddbd4ab598347fdd8a29e89696f70f91715b1d
geachgithub <getachewamogne4@gmail.com>
2023-07-26 07:50:52 -0300

    2-temperature file added

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,6 +1,6 @@
 #Tamprature converter function
 def convert_to_celsius(fahrenheit):
-    return ((fahrenheit-32)*(5/9))
+    return round(number=((fahrenheit-32)*(5/9)),ndigits=1 or 2)
 '''print(convert_to_celsius(100))
 print(convert_to_celsius(-40))
 print(convert_to_celsius(-459.67))
