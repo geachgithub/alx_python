256f2e27d63607c7039012b68d24f53c5e7a48e0
geachgithub <getachewamogne4@gmail.com>
2023-07-27 05:29:38 -0300

    edges added

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,4 +1,6 @@
-
+word_first_3:str
+word_last_2:str
+middle_word:str
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
