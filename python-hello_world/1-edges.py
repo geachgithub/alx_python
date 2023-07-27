1c49852e0aa4d30cbb3bec0999460eedeb274f10
geachgithub <getachewamogne4@gmail.com>
2023-07-27 05:36:33 -0300

    edges added

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,7 +1,4 @@
 word="Holberton"
-word_first_3:str
-word_last_2:str
-middle_word:str
-print("First 3 letters: {}".format(word_first_3))
-print("Last 2 letters: {}".format(word_last_2))
-print("Middle word: {}".format(middle_word))
+print("First 3 letters: {}".format(word[:3]))
+print("Last 2 letters: {}".format(word[7:]))
+print("Middle word: {}".format(word[1:8]))
