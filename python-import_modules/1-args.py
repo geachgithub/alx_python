2a9c60758a2b8d5b2887bd10690ecd9d0e5660fe
geachgithub <getachewamogne4@gmail.com>
2023-07-31 15:10:30 -0300

    python import modules

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,14 @@
+import sys
+l=len(sys.argv)-1
+if(l==0):
+    print('{} arguments.'.format(l), end='\n')
+elif(l==1):
+        print('{} argument:'.format(l))
+        print('{}:{}'.format(l,sys.argv[l]), end='\n')
+else :
+    m=1
+    print('{} arguments:'.format(l), end='\n')
+    for i in sys.argv:
+         if i!=sys.argv[0]:
+            print('{}:{}'.format(m,i), end='\n')
+            m+=1
---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,18 @@
+
+def safe_print_division(a,b):
+    c=0
+    try:
+       c = a/b
+    except ZeroDivisionError:
+        print('It is impossible to devide a number by zero')
+    finally:
+        if b!=0:
+         print('Inside result:{}'.format(c))
+         print('{}/{}={}'.format(a,b,c))
+         return c
+        else :
+            print('Inside result:{}'.format('None'))
+            print('{}/{}={}'.format(a,b,'None'))
+
+           
+safe_print_division(1,-1)
