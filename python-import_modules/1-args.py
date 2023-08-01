cfc958749c3c4daf42ff495ef59dcbc2f9c7dd16
geachgithub <getachewamogne4@gmail.com>
2023-08-01 07:53:10 -0300

    1-args can not be printed

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -2,15 +2,14 @@ import sys
 l=len(sys.argv)-1
 if __name__=="__main__":
     if(l==0):
-
         print('{} arguments.'.format(l), end='\n')
     elif(l==1):
             print('{} argument:'.format(l))
-            print('{}:{}'.format(l,sys.argv[l]), end='\n')
+            print('{}: {}'.format(l,sys.argv[l]), end='\n')
     else :
         m=1
         print('{} arguments:'.format(l), end='\n')
         for i in sys.argv:
             if i!=sys.argv[0]:
-                print('{}:{}'.format(m,i), end='\n')
+                print('{}: {}'.format(m,i), end='\n')
                 m+=1
