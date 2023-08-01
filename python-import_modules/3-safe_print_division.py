50b19e6924893b893bc643da28d8925b9ba4dd2f
geachgithub <getachewamogne4@gmail.com>
2023-08-01 08:07:45 -0300

    safe division and printing

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -8,11 +8,11 @@ def safe_print_division(a,b):
     finally:
         if b!=0:
          print('Inside result: {}'.format(c))
-         print('{}/{}={}'.format(a,b,c))
+         #print('{}/{}={}'.format(a,b,c))
          return c
         else :
             print('Inside result: {}'.format('None'))
-            print('{}/{}={} '.format(a,b,'None'))
+            #print('{}/{}={} '.format(a,b,'None'))
 
            
 #safe_print_division(1,-1)
