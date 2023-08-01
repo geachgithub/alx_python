6621b21eab478adc643ad769d7520fbb091bacb1
geachgithub <getachewamogne4@gmail.com>
2023-08-01 10:46:28 -0300

    0-no_c.py file added

---
+++ python-data_structures/0-no_c.py
@@ -0,0 +1,14 @@
+new_string=[]
+c=0
+def no_c(my_string):
+    l=len(my_string)
+    for i in range(l):
+        if(my_string[i]=='c' or my_string[i]=='C'):
+            
+            my_string[i]
+        else:
+            letter=my_string[i]
+            new_string.append(letter)
+    for i in new_string:
+        print(i,end='')     
+       
