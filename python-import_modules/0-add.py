6550b3f9c1bd2586f18694cc3122f03a6e8818b4
geachgithub <getachewamogne4@gmail.com>
2023-08-01 07:33:29 -0300

    add file added

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,5 @@
-
+from add_0 import add
 a=1
 b=2
-from add_0 import add
-c=add(a,b)
 if __name__=="__main__":
-    print('{}+{}={}'.format(a,b,c),end="\n")
+    print('{}+{}={}'.format(a,b,add(a,b)),end="\n")
