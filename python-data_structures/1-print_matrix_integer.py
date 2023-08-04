8fe3d7327c636a113d2724824155ab36d3578657
geachgithub <getachewamogne4@gmail.com>
2023-08-04 06:47:05 -0300

    printing matrix of numbers in python

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,8 +1,7 @@
 def print_matrix_integer(matrix):
-    print(matrix)
-matrix=[[1,2,3],[4,5,6],[7,8,9]]
-for j in range(len(matrix)):
-    for i in range(len(matrix)):
-        print('{:d} '.format(matrix[i][j]), end="")
-    print('\n')
+    #matrix=[[1,2,3],[4,5,6],[7,8,9]]
+    for j in range(len(matrix)):
+        for i in range(len(matrix)):
+            print('{:d} '.format(matrix[i][j]), end="")
+        print('\n')
     
