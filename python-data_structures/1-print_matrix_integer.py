e223a365a99f1de590bd10c8d932033817ca3e63
geachgithub <getachewamogne4@gmail.com>
2023-08-07 04:14:29 -0300

    matrix of numbers

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix):
         for j in range(len(matrix[i])):
             if i==len(matrix[i]):
                 final=''
-            print('{:d}{}'.format(matrix[i][j]),final,end="")
+            print('{:d}{}'.format(matrix[i][j],final),end="")
         print(end='\n')
-'''matrix=[[1,2,3],[4,5,6],[7,8,9]]
-print_matrix_integer(matrix)'''
+matrix=[[1,2,3],[4,5,6],[7,8,9]]
+print_matrix_integer(matrix)
