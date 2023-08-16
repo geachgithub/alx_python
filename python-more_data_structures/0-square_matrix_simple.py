38fb46a8aa0158631068c9e8e33ace24808603e8
geachgithub <getachewamogne4@gmail.com>
2023-08-15 22:40:57 -0900

    readme file added

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,15 +1,17 @@
 def square_matrix_simple(matrix):
     squared=[]
     transposed=[]
-    for i in range(len(matrix)):
+    m=0
+    for i in range(len(matrix[m])):
         squared.append([row[i]**2 for row in matrix])   
+        m+=1
     for i in range(len(matrix)):
         transposed.append([row[i] for row in squared]) 
 
-    return(transposed)   
-    '''print(len(matrix))
+    '''#return(transposed)   
+    print(len(matrix))
     print(matrix)
-    #print(squared)
+    print(squared)
     print(transposed)
-matrix=[[1,2,3],[4,5,6],[7,8,9]]
+matrix=[[1,2],[4,5],[7,8]]
 square_matrix_simple(matrix)'''
